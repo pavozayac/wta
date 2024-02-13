58ba8fdd51171ff2e983b38e6c946bdee859ee28
Paweł Zając <pavlzayac@gmail.com>
2024-02-13 19:41:24 +0100

    Add project and API key service

---
+++ wta/api/access_service.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod, abstractclassmethod
+# from os import environ
+import os
+
+class ApiAccessService(ABC):
+    @abstractmethod
+    def api_key(self) -> str:
+        pass
+
+class EnvApiAccessService(ApiAccessService):
+
+    def __init__(self, var_name = 'WTA_API_KEY') -> None:
+        self.ENV_VAR_NAME = var_name
+    
+    def api_key(self) -> str:
+        if self.ENV_VAR_NAME not in os.environ:
+            raise KeyError(f'Environment variable {self.ENV_VAR_NAME} was not found.')
+
+        return os.environ[self.ENV_VAR_NAME]
