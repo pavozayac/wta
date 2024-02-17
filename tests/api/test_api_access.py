b16c3af5f1e10515340e604afcf57f1e822bce06
pavozayac <pavlzayac@gmail.com>
2024-02-17 19:38:43 +0100

    Separate coverage into a separate action

--- tests/api/test_api_access.py
+++ tests/api/test_api_access.py
@@ -9,3 +9,11 @@ def test_should_get_key_from_env():
     sut = EnvApiAccessService()
 
     assert sut.api_key() == test_key
+
+def test_should_get_url_from_env():
+    test_url = 'example_url'
+    os.environ['WTA_BASE_URL'] = test_url
+
+    sut = EnvApiAccessService()
+
+    assert sut.base_api_url() == test_url
