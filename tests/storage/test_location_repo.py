f2c850b77c37181134bd540787b6ddcea8a453d7
Paweł Zając <pavlzayac@gmail.com>
2024-02-19 23:05:36 +0100

    Fix bug in a test

--- tests/storage/test_location_repo.py
+++ tests/storage/test_location_repo.py
@@ -3,8 +3,8 @@ from wta.storage.location_repo import JSONLocationRepository
 
 def test_should_return_empty_on_not_found():
     weird_file = './abcedefghijklmnop.bin'
-    loc_repo = JSONLocationRepository()
+    loc_repo = JSONLocationRepository(weird_file)
 
-    result = loc_repo.get_locations(weird_file).bus_dict
+    result = loc_repo.get_locations().bus_dict
 
     assert len(result) == 0
